0ac925ecb2e3be859f26d888f449a23749170d17
luke1188 <49883653+luke1188@users.noreply.github.com>
2023-09-15 16:40:30 -0400

    actually works now

--- src/extension.ts
+++ src/extension.ts
@@ -4,30 +4,29 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+const settings = vscode.workspace.getConfiguration('rust-analyzer');
+const defaultValue = settings.get('cargo.target');
+const target = 'wasm32-unknown-unknown';
+let currentValue= defaultValue;
+
+
 export function activate(context: vscode.ExtensionContext) {
 
+	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "rust-target-toggle" is now active!');
-	const settings = vscode.workspace.getConfiguration('rust-analyzer');
-	const currentValue = settings.get('cargo.target');
-	const target = 'wasm32-unknown-unknown';
-
-	// button 
-	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-    statusBarItem.text = "Target: " + currentValue;
-    statusBarItem.tooltip = "Toggle Rust Target";
-    statusBarItem.command = 'rust-target-toggle.toggleTarget';
-    statusBarItem.show();
-    context.subscriptions.push(statusBarItem);
+	
+	
 
 	let disposable = vscode.commands.registerCommand('rust-target-toggle.toggleTarget', async  () => {
 		// if current val is wasm
+		console.log("current setting: " + currentValue);
 		if (currentValue === target) {
 			try{
 			 	await settings.update('cargo.target', undefined, vscode.ConfigurationTarget.Workspace);
 				vscode.window.showInformationMessage('Removed target setting, you can ignore the rust server restart');
-				
+
 				//restart server
 				await vscode.commands.executeCommand('rust-analyzer.restartServer'); 
 			}
@@ -36,6 +35,8 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			finally{
 				vscode.window.showInformationMessage("Sucess");
+				currentValue = null;
+				console.log("leaving... " + settings.get('cargo.target'));
 			}
 		// else replace with wasm	
 		}  else {
@@ -51,15 +52,23 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			finally{
 				vscode.window.showInformationMessage("Success");
+				currentValue = target;
+				console.log("leaving... " + settings.get('cargo.target'));
 			}
 
 		}
 
 	});
+
+	// button
+	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    statusBarItem.text = "Target Toggle";
+    statusBarItem.tooltip = "Toggle Rust Target";
+    statusBarItem.command = 'rust-target-toggle.toggleTarget';
+    statusBarItem.show();
+    context.subscriptions.push(statusBarItem);
+	
 	vscode.workspace.getConfiguration('rust-analyzer');
 	context.subscriptions.push(disposable);
 
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
